8a76ed035a48004c5775fc570b3f66649ffdfcc3
Jakub Gedeon <kazagistar@gmail.com>
2017-12-03 23:31:56 -0700

    part 4 add tests

--- src/day4.rs
+++ src/day4.rs
@@ -13,9 +13,22 @@ pub fn part2(input: &str) -> usize {
     })
 }
 
-fn count_uniques<'a, Key: Ord + Hash + Eq + 'a>(input: &'a str, key: impl Fn(&'a str) -> Key) -> usize {
+fn count_uniques<'a, Key: Ord + Hash + 'a>(input: &'a str, key: impl Fn(&'a str) -> Key) -> usize {
     input.lines().filter(|passphrase| {
         let mut dupes = HashSet::new();
         passphrase.split_whitespace().flat_map(|word| dupes.replace(key(word))).count() == 0
     }).count()
+}
+
+#[test]
+fn examples() {
+    assert_eq!(1, part1("aa bb cc dd ee"));
+    assert_eq!(0, part1("aa bb cc dd aa"));
+    assert_eq!(1, part1("aa bb cc dd aaa"));
+
+    assert_eq!(1, part2("abcde fghij"));
+    assert_eq!(0, part2("abcde xyz ecdab"));
+    assert_eq!(1, part2("a ab abc abd abf abj"));
+    assert_eq!(1, part2("iiii oiii ooii oooi oooo"));
+    assert_eq!(0, part2("oiii ioii iioi iiio"));
 }
