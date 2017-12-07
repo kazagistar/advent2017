731166b5be471d0bdcb4f134c1ece803e43d6e31
Jakub Gedeon <kazagistar@gmail.com>
2017-12-07 01:07:38 -0700

    day 7 tests

--- src/day7.rs
+++ src/day7.rs
@@ -11,7 +11,7 @@ pub fn part2(input: &str) -> usize {
 }
 
 lazy_static! {
-    static ref MATCHER: Regex = Regex::new(r"^(\w+) \((\d+)\)(?: -> (.*))?$").unwrap();
+    static ref MATCHER: Regex = Regex::new(r"(\w+) \((\d+)\)(?: -> (.*))?").unwrap();
 }
 
 type Weight = usize;
@@ -69,4 +69,24 @@ fn find_bad(programs: &HashMap<&str, Program>, name: &str) -> Result<Weight, Wei
     let difference = good_weight - bad_weight;
     let fixed = programs.get(bad[0]).unwrap().weight + difference;
     Err(fixed)
+}
+
+#[test]
+fn examples() {
+    let input = "pbga (66)
+                 xhth (57)
+                 ebii (61)
+                 havc (66)
+                 ktlj (57)
+                 fwft (72) -> ktlj, cntj, xhth
+                 qoyq (66)
+                 padx (45) -> pbga, havc, qoyq
+                 tknk (41) -> ugml, padx, fwft
+                 jptl (61)
+                 ugml (68) -> gyxo, ebii, jptl
+                 gyxo (61)
+                 cntj (57)";
+
+    assert_eq!("tknk", part1(input));
+    assert_eq!(60, part2(input));
 }
